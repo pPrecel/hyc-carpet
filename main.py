b8e97142b836a4f7416010ae59393b244ccc768d
pPrecel <filip.strozik@outlook.com>
2020-11-24 11:25:13 +0100

    Rebuild project

--- main.py
+++ main.py
@@ -5,43 +5,29 @@ import matplotlib.pyplot as plt
 from matplotlib import colors
 from flask import Flask
 from flask import request
-import numpy as np
+import numpy
  
-def carpet(degree):
+def calculate(degree):
     size=3**degree
-    matrix=np.ones([size, size])
-
-    for niveau in range(degree+1):
-        step=3**(degree-niveau)
+    matrix=numpy.ones([size, size])
+    for elem in range(degree+1):
+        step=3**(degree-elem)
         for x in range(size):
             if x%3==1:
                 for y in range(size):
                     if y%3==1:
                         matrix[y*step:(y+1)*step, x*step:(x+1)*step]=0
-                
     return matrix
 
 def render_carpet(degree):
-    matrix=carpet(degree)
-    zoom=1 
-    bouge=[
-            0
-            ,0
-    ]
+    matrix=calculate(degree)
 
-    matplotlib.rcParams['toolbar'] = 'None'
-
-
-    fig=plt.figure(dpi=72,frameon=False)
-
-    cmap = 'gray'
+    fig=plt.figure()
 
     ax = plt.Axes(fig, [0., 0., 1., 1.])
-    ax.set_axis_off()
     fig.add_axes(ax)
 
-    img=ax.imshow(matrix, cmap, interpolation="bilinear")
-
+    img=ax.imshow(matrix)
     return mpld3.fig_to_html(fig)
 
 app = Flask(__name__)
@@ -49,6 +35,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     degree = request.args.get('degree', default = 6, type = int)
+    print("Degree:", degree)
     return render_carpet(degree)
 
 if __name__ == '__main__':
